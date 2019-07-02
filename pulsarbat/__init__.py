3863586d165c39ff89886eda2292f26d53498e0a
Nikhil Mahajan <mahajan@astro.utoronto.ca>
2019-07-02 06:56:44 -0400

    Fixed email, version, small typos

--- pulsarbat/__init__.py
+++ pulsarbat/__init__.py
@@ -1,12 +1,8 @@
 """Top-level package for pulsarbat."""
 
 __author__ = """Nikhil Mahajan"""
-__email__ = 'nikhilm92@gmail.com'
+__email__ = 'mahajan@astro.utoronto.ca'
 __version__ = '0.0.2'
-__all__ = []
 
-from . import core
-from . import predictor
-
-__all__.extend(core.__all__)
-__all__.extend(predictor.__all__)
+from .core import BasebandSignal
+from .predictor import Polyco
--- pulsarbat/core.py
+++ pulsarbat/core.py
@@ -18,11 +18,13 @@ class BasebandSignal:
     """Baseband signal class.
 
     Signals are in their complex baseband representation (the spectrum
-    must be centered on zero). Time is axis 0, the rest is the sample
-    shape. The first axis of sample shape (axis 1) is frequency.
+    must be centered on zero frequency). Time is axis 0, the rest is the
+    sample shape. The first axis of sample shape (axis 1) is frequency,
+    representing channels or bands.
     """
 
-    def __init__(self, z, sample_rate=None, center_freq=None):
+    def __init__(self, z, sample_rate=None, center_freq=None,
+                 dtype=np.complex64):
         if not isinstance(sample_rate, u.Quantity):
             raise TypeError('sample_rate must be an astropy Quantity.')
         if not isinstance(center_freq, u.Quantity):
@@ -33,7 +35,7 @@ class BasebandSignal:
         if not center_freq.unit.is_equivalent(u.Hz):
             raise u.UnitTypeError('center_freq must have units of frequency.')
 
-        self._z = np.asarray(z).astype(np.complex64)
+        self._z = np.asarray(z).astype(dtype)
         self._sample_rate = sample_rate
         self._center_freq = center_freq
 
@@ -43,10 +45,6 @@ class BasebandSignal:
     @property
     def dt(self):
         return (1 / self._sample_rate).to(u.s)
-
-    @property
-    def data(self):
-        return self._z
 
     @property
     def sample_rate(self):
@@ -66,4 +64,3 @@ class BasebandReader:
 
         self.start_time = Time(self.fh.start_time, format='isot', precision=9)
         self.stop_time = Time(self.fh.stop_time, format='isot', precision=9)
-        pass
