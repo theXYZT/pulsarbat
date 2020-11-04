05adf16692227dd4b63d91e4e0cc4596dc917a28
Rebecca Lin <13529540+00rebe@users.noreply.github.com>
2020-11-03 19:57:23 -0500

    Added incoherent dedispersion function (#29)

--- pulsarbat/dedispersion.py
+++ pulsarbat/dedispersion.py
@@ -97,4 +97,41 @@ def coherent_dedispersion(z, DM, /, *, ref_freq, chirp=None):
 
 
 def incoherent_dedispersion(z, DM, /, *, ref_freq):
-    raise NotImplementedError("Will do it soon, I promise!")
+    """Incoherently dedisperses a signal by a given dispersion measure.
+
+    The output signal will be cropped on both ends to avoid wrap-around
+    artifacts caused by dedispersion. This depends on how the reference
+    frequency (`ref_freq`) compares to the band of the signal.
+
+    Parameters
+    ----------
+    z : `~pulsarbat.RadioSignal`
+        The signal to be transformed.
+    DM : `~pulsarbat.DispersionMeasure`
+        Dispersion measure by which to dedisperse `z`.
+    ref_freq : `~astropy.units.Quantity`
+        Reference frequency to dedisperse to.
+
+    Returns
+    -------
+    out : `~pulsarbat.RadioSignal`
+        The dedispersed signal.
+    """
+    if not isinstance(z, RadioSignal):
+        raise TypeError("Signal must be a RadioSignal object.")
+
+    delays = DM.sample_delay(z.channel_freqs, ref_freq, z.sample_rate)
+    delays = delays.round().astype(int)
+
+    crop_before = -min(0, delays[0], delays[-1])
+    delays += crop_before
+    N = len(z) - max(delays)
+
+    x = np.stack([z.data[j:j+N, i] for i, j in enumerate(delays)], axis=1)
+
+    if crop_before and z.start_time is not None:
+        new_start = z.start_time + crop_before * z.dt
+    else:
+        new_start = z.start_time
+
+    return type(z).like(z, x, start_time=new_start)
