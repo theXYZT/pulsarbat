0ded05ed52dd4a9138905aedbf67951ca0e1d759
Nikhil Mahajan <nikhilm92@gmail.com>
2022-10-07 17:08:52 -0400

    Updated Rebecca's ORCID and updated some requirements.

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ setup(
     name="pulsarbat",
     install_requires=[
         "astropy >= 5.1",
-        "numpy >= 1.22",
+        "numpy >= 1.23",
         "scipy >= 1.9",
         "baseband >= 4.1.1",
-        "dask[array] >= 2022.8.1",
+        "dask[array] >= 2022.9.2",
     ],
-    tests_require=["pytest >= 7.1.2", "cloudpickle >= 2.1.0"],
+    tests_require=["pytest >= 7.1.3", "cloudpickle >= 2.2.0"],
 )
